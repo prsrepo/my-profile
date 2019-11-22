21963929fc6a9cef417b68c7f9abf5be2bac57cb
Purushotham Reddy <puroshotham.k@noodle.ai>
2019-11-22 11:14:22 +0530

    #packaged flask app

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='my_profile',
+    version='0.0.1',
+    description='This library will give the details of my profile',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prsrepo/my-profile",
+    author='puru',
+    author_email='kspreddy94@gmail.com',
+    packages=['my_profile'],
+    install_requires=[
+        'Flask==1.1.0'
+    ],
+    python_requires='>=3.6'
+)
